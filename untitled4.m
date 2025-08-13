cb93acc0748e8a7f6ed1b00fe5e5ae46518420e3
Heitorccf <Heitorccfernandes550@gmail.com>
2025-08-13 08:09:01 -0300

    primeiro commit

---
+++ untitled4.m
@@ -0,0 +1,28 @@
+% Ler a imagem original
+imagemOriginal = imread('Imagens/if.jpg');
+
+% Converter a imagem para tons de cinza
+imagemCinza = rgb2gray(imagemOriginal);
+
+% Redimensionar a imagem para 70% do tamanho original
+imagemRedimensionada = imresize(imagemOriginal, 0.7);
+imagemCinzaRedimensionada = imresize(imagemCinza, 0.7);
+
+% Exibir as imagens com títulos
+figure;
+
+subplot(2, 2, 1);
+imshow(imagemOriginal);
+title('Imagem Original');
+
+subplot(2, 2, 2);
+imshow(imagemCinza);
+title('Imagem em Tons de Cinza');
+
+subplot(2, 2, 3);
+imshow(imagemRedimensionada);
+title('Imagem Redimensionada (70%)');
+
+subplot(2, 2, 4);
+imshow(imagemCinzaRedimensionada);
+title('Imagem Redimensionada em Tons de Cinza (70%)');
