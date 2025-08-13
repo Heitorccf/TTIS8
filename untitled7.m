dad4249c11f4ea2444cb58b7af3fb808caee2190
Heitorccf <Heitorccfernandes550@gmail.com>
2025-08-13 09:19:50 -0300

    13/08

---
+++ untitled7.m
@@ -0,0 +1,19 @@
+% Carregar a imagem
+img = imread('Imagens/lena.bmp');
+
+% Definir o retângulo de corte [x, y, largura, altura]
+retanguloCorte = [100, 100, 200, 200]; % Ajuste os valores conforme necessário
+
+% Realizar o corte da imagem
+imgCortada = imcrop(img, retanguloCorte);
+
+% Exibir a imagem original e a cortada lado a lado
+figure;
+
+subplot(1, 2, 1);
+imshow(img);
+title('Imagem Original');
+
+subplot(1, 2, 2);
+imshow(imgCortada);
+title('Imagem Cortada');
